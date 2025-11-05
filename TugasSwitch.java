acb30eec0df8ff0e32fccee517937c98d75f29da
muhammadsafarudinfirmani <muhammadsafarudinfirmani@gmail.com>
2025-11-05 16:47:30 +0700

    melakukan sistem input ouput

--- TugasSwitch.java
+++ TugasSwitch.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class TugasSwitch {
 
     public static void main(String[] args) {
+        System.out.println("=====PROGAM ASISTEN DOSAN=====");
         double struktur_data, pbObjek, Basis_Data, algoritma_pemrogaman, rpLunak;
 
-        Scanner ns = new Scanner(System.in)
+        Scanner ns = new Scanner(System.in);
+        System.out.print("Masukkan nilai struktur data :");
+        struktur_data = ns.nextDouble();
+        System.out.print("Pemrogaman berorientasi objek :");
+        pbObjek= ns.nextDouble();
+        System.out.print("Basis data :");
+        Basis_Data= ns.nextDouble();
+        System.out.print("Algoritma pemrogaman :");
+        algoritma_pemrogaman= ns.nextDouble();
+        System.out.print("Rekayasa perangkat lunak :");
+        rpLunak= ns.nextDouble();
     }
 }
