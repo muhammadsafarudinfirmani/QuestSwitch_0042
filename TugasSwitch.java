1b4cf2eda8f0087e638bfd5b2b158e62ec0bc1a4
muhammadsafarudinfirmani <muhammadsafarudinfirmani@gmail.com>
2025-11-05 16:32:49 +0700

    menambahkan Scanner

--- TugasSwitch.java
+++ TugasSwitch.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class TugasSwitch {
 
     public static void main(String[] args) {
         double struktur_data, pbObjek, Basis_Data, algoritma_pemrogaman, rpLunak;
+
+        Scanner ns = new Scanner(System.in)
     }
 }
